bd33059c09b5fe5ffcd8379b924e66004ed861af
Asliddin <asliddinabijonov04@gmail.com>
2022-05-30 09:40:53 -0700

    'yakshanba'

--- src/components/causes/index.js
+++ src/components/causes/index.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import { Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
 
 function Causes() {
+  const data = [
+    {
+      img: require("../../assets/yurak.png"),
+      text: "CLEAN WATER",
+    },
+    {
+      img: require("../../assets/tomchi.png"),
+      text: "SPONSORSHIP",
+    },
+    {
+      img: require("../../assets/love.png"),
+      text: "HEALTH PRO",
+    },
+    {
+      img: require("../../assets/book.png"),
+      text: "BOOKS",
+    },
+  ];
   return (
-    <div>Causes</div>
-  )
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 600,
+            fontSize: 30,
+          }}
+        >
+          OUR CAUSES
+        </Typography>
+        <Typography
+          sx={{ fontSize: 15, color: "#707070", pt: { md: "1.5%", sm: "5%" } }}
+        >
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the <br /> industry's standard dummy
+          text ever since the 1500s, when an unknown printer took a galley of
+          type <br /> and scrambled it to make a type specimen book.
+        </Typography>
+      </Box>
+      <Box sx={{ mt: { md: "3%", sm: "4%", xs: "8%" } }}>
+        <Grid
+          container
+          rowSpacing={{ sm: 4, xs: 5 }}
+          columnSpacing={{ md: 5, sm: 5 }}
+        >
+          {data.map((rasm, num) => (
+            <Grid key={num} xs={12} sm={6} md={3} lg={3} xl={3} item>
+              <Box sx={{ textAlign: "center" }}>
+                <img src={rasm.img} />
+                <Typography
+                  sx={{ fontWeight: 550, fontSize: 14, pt: "6%", pb: "4%" }}
+                >
+                  {rasm.text}
+                </Typography>
+                <Typography sx={{ fontSize: 15, color: "#707070", pb: "4%" }}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been.
+                </Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Box sx={{ pb: "10%" }} />
+    </Container>
+  );
 }
 
-export default Causes
+export default Causes;
--- src/components/charity/index.js
+++ src/components/charity/index.js
@@ -1,9 +1,200 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Container,
+  Grid,
+  Tab,
+  Typography,
+  Tabs,
+  Button,
+} from "@mui/material";
+import bgimg from "../../assets/bg.jpg";
+import PropTypes from "prop-types";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 
 function Charity() {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>Charity</div>
-  )
+    <>
+      <Grid container>
+        <Grid xs={12} sm={12} md={12} lg={6} xl={6} item>
+          <img src={bgimg} style={{ width: "100%", height: "100%" }} />
+        </Grid>
+        <Grid
+          xs={12}
+          sm={12}
+          md={12}
+          lg={6}
+          xl={6}
+          item
+          sx={{
+            bgcolor: "#FF573D",
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box sx={{ pt: "10%", pl: "10%", pr: "10%" }}>
+            <Typography sx={{ fontSize: { md: 35, sm: 30, xs: 23 } }}>
+              Big Charity Fund For Childs.
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: { md: 35, sm: 30, xs: 25 },
+                fontWeight: 200,
+                lineHeight: 1,
+              }}
+            >
+              Donate & support our work <br /> today.
+            </Typography>
+            <br />
+            <br />
+            <hr />
+            <TabContext value={value}>
+              <Box>
+                <TabList
+                  onChange={handleChange}
+                  // aria-label="lab API tabs example"
+                >
+                  <Tab sx={{ color: "white" }} label="Item One" value="1" />
+                  <Tab sx={{ color: "white" }} label="Item Two" value="2" />
+                  <Tab sx={{ color: "white" }} label="Item Three" value="3" />
+                  <Tab sx={{ color: "white" }} label="Item Three" value="4" />
+                </TabList>
+              </Box>
+              <TabPanel value="1" sx={{ fontSize: 15 }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                <br />
+                <Button
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: 20,
+                    pl: "5%",
+                    pr: "5%",
+                    color: "red",
+                  }}
+                >
+                  READ MORE >
+                </Button>
+              </TabPanel>
+              <TabPanel value="2" sx={{ fontSize: 15 }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                <br />
+                <Button
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: 20,
+                    pl: "5%",
+                    pr: "5%",
+                    color: "red",
+                  }}
+                >
+                  READ MORE >
+                </Button>
+              </TabPanel>
+              <TabPanel value="3" sx={{ fontSize: 15 }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                <br />
+                <Button
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: 20,
+                    pl: "5%",
+                    pr: "5%",
+                    color: "red",
+                  }}
+                >
+                  READ MORE >
+                </Button>
+              </TabPanel>
+              <TabPanel value="4" sx={{ fontSize: 15 }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text. Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the standard dummy
+                text.
+                <br />
+                <br />
+                <br />
+                <Button
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: 20,
+                    pl: "5%",
+                    pr: "5%",
+                    color: "red",
+                  }}
+                >
+                  READ MORE >
+                </Button>
+              </TabPanel>
+            </TabContext>
+          </Box>
+        </Grid>
+      </Grid>
+      ;
+    </>
+  );
 }
 
-export default Charity
+export default Charity;
